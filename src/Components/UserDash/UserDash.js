1ea2a75960bd21f42728663f18d96b64f3a86e84
Gladi023 <Gladimar.Villamil@gmail.com>
2023-01-13 15:02:29 -0800

    readme updated

--- src/Components/UserDash/UserDash.js
+++ src/Components/UserDash/UserDash.js
@@ -7,22 +7,22 @@ const UserDash = () => {
             <div className='tc grow br2 pa4 dib'>
                 <img alt='avatar' src={avatar} class="o-60 br-100 h5 w5 dib" />
             </div>
-               <p className='f2 white'>
-                {'Welcome to my React Portfolio'}
+               <p className='f2 black underline'>
+                {'Gladimar Villamil react portfolio'}
             </p>
-              <div className='white f2' id="about-me" >
+              <div className='black f2 underline' id="about-me" >
                 {'About Me:'}
             </div>
             <br></br>
-            <div className='white f3'>
+            <div className='black f3'>
                 {'Hello, my Name I Gladimar Villamil, I work in entertainment, in legal and development'}
             </div>
-            <div className='white f3'>
+            <div className='black f3'>
                 {'and I am passionate about producing quality entertainment for online platforms and creating beautiful and innovatine user interfaces.'}
             </div>
             <br></br>
             <br></br>
-            <div className='white f2'>
+            <div className='black f3 underline'>
                 {'Find some of my web applications below.'}
             </div>
         </div>
